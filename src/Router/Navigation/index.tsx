bfac68f229c9eb3f41856c6c29c5777ffc477d16
DewaSRY <sdewa6645@gmail.com>
2023-07-27 09:41:49 +0800

    update Rout structure

--- src/Router/Navigation/index.tsx
+++ src/Router/Navigation/index.tsx
@@ -1,10 +1,77 @@
-import { Crwn } from "../../Assets";
-import { CartDropDown } from "./CartDropDown";
-import { CartIcon } from "./CartIcon";
+import style from "./style/Navigation.module.scss";
+import { Crwn, ShoppingBags } from "../../Assets";
 import { Outlet, Link } from "react-router-dom";
 import { signOutUser } from "../../utils/Firebase.utils";
-import style from "./style/Navigation.module.scss";
 import { useUser, useCart } from "../../hooks";
+import { useNavigate } from "react-router-dom";
+import { Button } from "../../component/Button";
+interface ICartItems {
+  cartItem: {
+    name: string;
+    quantity: number;
+    imageUrl: string;
+    price: number;
+  };
+}
+const SignInLink = () => (
+  <Link className={style["nav-link"]} to="Authentication">
+    SIGN IN
+  </Link>
+);
+
+const SignOutLink = () => (
+  <span className={style["nav-link"]} onClick={signOutUser}>
+    SIGN OUT
+  </span>
+);
+const CartItem = ({ cartItem }: ICartItems) => {
+  const { name, quantity, imageUrl, price } = cartItem;
+  return (
+    <div className={style["cart-item-container"]}>
+      <img src={imageUrl} alt={name} />
+      <div className={style["item-details"]}>
+        <h2 className={style.name}>{name}</h2>
+        <span className={style.price}>
+          {quantity} x ${price}
+        </span>
+      </div>
+    </div>
+  );
+};
+const CartIcon = () => {
+  const { cartCount, setIsCartOpen } = useCart();
+  const navigate = useNavigate();
+  function CartHalle() {
+    if (!cartCount) {
+      return navigate("/checkout");
+    }
+    setIsCartOpen();
+  }
+  return (
+    <div className={style["cart-icon-container"]} onClick={CartHalle}>
+      <ShoppingBags className={style["shopping-icon"]} />
+      <span className={style["item-count"]}>{cartCount}</span>
+    </div>
+  );
+};
+
+const CartDropDown = () => {
+  const { cartItems, setIsCartOpen } = useCart();
+  const navigate = useNavigate();
+  const goToCheckoutHandler = () => {
+    navigate("/checkout");
+    setIsCartOpen();
+  };
+  const CartDropdownItems = cartItems.map((item) => (
+    <CartItem key={item.id} cartItem={item} />
+  ));
+  return (
+    <div className={style["cart-dropdown-container"]}>
+      <div className={style["cart-items"]}>{CartDropdownItems}</div>
+      <Button onClick={goToCheckoutHandler}>CHECKOUT</Button>
+    </div>
+  );
+};
 export function Navigation() {
   const { currentUser } = useUser();
   const { isCartOpen } = useCart();
@@ -23,22 +90,7 @@ export function Navigation() {
         </div>
         {isCartOpen && <CartDropDown />}
       </div>
-
       <Outlet />
     </>
-  );
-}
-function SignInLink() {
-  return (
-    <Link className={style["nav-link"]} to="Authentication">
-      SIGN IN
-    </Link>
-  );
-}
-function SignOutLink() {
-  return (
-    <span className={style["nav-link"]} onClick={signOutUser}>
-      SIGN OUT
-    </span>
   );
 }
--- src/Router/Shop/index.tsx
+++ src/Router/Shop/index.tsx
@@ -1,6 +1,75 @@
-import { Routes, Route } from "react-router-dom";
-import { Previews } from "./Previews";
-import { Products } from "./Products";
+import style from "./style/Shop.module.scss";
+import { Routes, Route, Link } from "react-router-dom";
+import { Button, BUTTON_TYPE_CLASSES } from "../../component";
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { useProducts } from "../../hooks";
+
+import { useCart } from "../../hooks";
+const Card = ({ items }) => {
+  const { addItems } = useCart();
+  const { imageUrl, price, name } = items;
+  return (
+    <div className={style["product-card-container"]}>
+      <img src={imageUrl} alt={name} />
+      <div className={style.footer}>
+        <span className={style.name}>{name}</span>
+        <span className={style.price}>{price}</span>
+      </div>
+      <Button
+        buttonType={BUTTON_TYPE_CLASSES.INVERTED}
+        onClick={() => addItems(items)}
+      >
+        Add Item
+      </Button>
+    </div>
+  );
+};
+
+const Previews = () => {
+  const { cataGoriesMap } = useProducts();
+  return (
+    <>
+      {Object.keys(cataGoriesMap).map((key) => {
+        const products = cataGoriesMap[key];
+        return <Preview key={key} title={key} products={products} />;
+      })}
+    </>
+  );
+};
+const Preview = ({ title, products }) => {
+  const previewCategory = products
+    .filter((_, idx) => idx < 4)
+    .map((product) => <Card key={product.id} items={product} />);
+
+  return (
+    <div className={style["category-preview-container"]}>
+      <Link to={title}>
+        <span className={style.title}>{title.toUpperCase()}</span>
+      </Link>
+      <div className={style.preview}>{previewCategory}</div>
+    </div>
+  );
+};
+
+const Products = () => {
+  const { cataGoriesMap } = useProducts();
+  const { category } = useParams();
+  const [products, setProducts] = useState(cataGoriesMap[category]);
+  useEffect(() => {
+    const product = cataGoriesMap[category];
+    setProducts(product);
+  }, [category, cataGoriesMap]);
+  return (
+    <>
+      <h2 className={style["category-title"]}>{category.toUpperCase()}</h2>
+      <div className={style["category-container"]}>
+        {products &&
+          products.map((product) => <Card key={product.id} items={product} />)}
+      </div>
+    </>
+  );
+};
 export function Shope() {
   return (
     <Routes>

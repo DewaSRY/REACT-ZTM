01e6bc854e8f916006f3c0040c9113aebaf1ddd4
DewaSRY <sdewa6645@gmail.com>
2023-08-01 08:27:50 +0800

    initiate first

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./index.css";
-import { ProductseProvider, UserProvider, CartProvider } from "./Store";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import "./index.css";
+import { store, persister } from "./state";
+// import { ProductseProvider, UserProvider, CartProvider } from "./Store";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <UserProvider>
-      <ProductseProvider>
-        <CartProvider>
-          <BrowserRouter>
-            <App />
-          </BrowserRouter>
-        </CartProvider>
-      </ProductseProvider>
-    </UserProvider>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persister}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
